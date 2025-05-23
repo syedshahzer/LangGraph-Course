f178199b51024b7f0ed5d14233c30614771e84a3
Syed Shahzer <syedshahzer@Syeds-MacBook-Air.local>
2025-05-23 15:44:58 +0530

    use openrouter model

--- Agents/Agent_Bot.py
+++ Agents/Agent_Bot.py
@@ -1,18 +1,27 @@
+from os import getenv
 from typing import TypedDict, List
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langgraph.graph import StateGraph, START, END
 from dotenv import load_dotenv # used to store secret stuff like API keys or configuration values
+from chat_openrouter import ChatOpenRouter
 
 load_dotenv()
-
+# print("OpenRouter API Key:", getenv("OPENROUTER_API_KEY"))  # For debugging, remove in production
 class AgentState(TypedDict):
     messages: List[HumanMessage]
 
-llm = ChatOpenAI(model="gpt-4o")
+# llm = ChatOpenAI(
+#     model_name='mistralai/devstral-small:free',
+#     openai_api_key=getenv("OPENROUTER_API_KEY"),
+#     openai_api_base="https://openrouter.ai/api/v1"
+# )
+llm = ChatOpenRouter(
+    model_name='mistralai/devstral-small:free'
+)    
 
 def process(state: AgentState) -> AgentState:
-    response = llm.invoke(state["messages"])
+    response = llm.invoke(state["messages"])  # call the model directly
     print(f"\nAI: {response.content}")
     return state
 
